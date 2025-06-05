814e5d56ec01b98df5d3935cf7073c58b654f466
designbiconsult <julian@designbi.com.br>
2025-06-05 16:01:52 -0300

    teste

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Body
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import sqlite3
