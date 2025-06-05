02a51a657564157a4074e957f660fb04feb1dd86
designbiconsult <julian@designbi.com.br>
2025-06-05 13:51:04 -0300

    exclusao data

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from typing import List, Dict
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Em produção, restrinja!
+    allow_origins=["http://localhost:3000"],  # Em produção, restrinja!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
