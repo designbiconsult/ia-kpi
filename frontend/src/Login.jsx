848bb637823da41042437cb642b0b7da6e7093fe
designbiconsult <julian@designbi.com.br>
2025-06-12 23:31:37 -0300

    altera cor

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -100,7 +100,7 @@ export default function Login({ onLogin }) {
               </Button>
               <Button
                 variant="text"
-                color="primary"
+                color="secondary"
                 onClick={gotoCadastro}
                 fullWidth
                 sx={{ mt: 0.5 }}
